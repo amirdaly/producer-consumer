6919db9306da27545b3b00c0db2d02a3452d5fcb
Amirdaly <amirdaly@yahoo.com>
2023-02-15 19:28:40 +0330

    added some description

--- main.go
+++ main.go
@@ -10,23 +10,35 @@ const NumberOfPizzas = 10
 
 var pizzasMade, pizzasFailed, total int
 
+// Producer is a type for structs that holds tow chammels: one for pizzas, with all
+// information for a given pizza order including whether it  was made
+// successfully, and another to handle end of processing (when quit the channel)
 type Producer struct {
 	data chan PizzaOrder
 	quit chan chan error
 }
 
+// PizzaOrder is a type for structs that describes a given pizza order. It has the order
+// number, a message indicating what happened to the order, and a boolean
+// indicating if the order was successfully completed.
 type PizzaOrder struct {
 	pizzaNumber int
 	message     string
 	success     bool
 }
 
+// Close is simply a method of closing the channel when we are done it (i.e)
+// something is pushed to the quit channel)
 func (p *Producer) Close() error {
 	ch := make(chan error)
 	p.quit <- ch
 	return <-ch
 }
 
+// makePizza attempts to make pizza. We generate a reandom number from 1-12,
+// and put in two cases where we can't make the pizza in time. Otherwise,
+// we make the pizza whithout issue. To make things intresting, each pizza
+// will take a different length ig time to produce (some pizzas are harder than others).
 func makePizza(pizzaNumber int) *PizzaOrder {
 	pizzaNumber++
 	if pizzaNumber <= NumberOfPizzas {
